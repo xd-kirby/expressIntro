9200f69abe12042dfefa7ddb960e21e297938c03
xd-kirby <alexhuica01@gmail.com>
2026-04-01 11:24:18 +0200

    Added route parameters and queries

--- server.js
+++ server.js
@@ -15,8 +15,28 @@ app.get("/users", (req, res) => {
   res.send("Here are some users... Nevermind there are no users.");
 });
 
-app.get("/message", (req, res) => {
-  res.send("This is a message from message page on the express server.");
+// dynamic route with a parameter "name"
+app.get("/hello/:name", (req, res) => {
+  res.send(`Hello, ${req.params.name}!`);
+});
+
+// dynamic route with a parameter "id"
+app.get("/product/:id", (req, res) => {
+  console.log(req.params);
+  res.send(`This is the product page for product with ID: ${req.params.id}`);
+});
+
+// query parameters example: /search?q=keyword
+app.get("/search", (req, res) => {
+  console.log(req.query.q);
+  res.send(`You searched for: ${req.query.q}`);
+});
+
+// multiple query parameters example: /filter?category=electronics&price=100
+app.get("/filter", (req, res) => {
+  res.send(
+    `Filtering products by category: ${req.query.category} and is sorted by: ${req.query.price}`,
+  );
 });
 
 app.listen(PORT, () => {
